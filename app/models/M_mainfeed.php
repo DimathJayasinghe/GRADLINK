29cde3a87bf33faba2987a902e0817e614ce457c
DimathJayasinghe <dimathjayasinghe@gmail.com>
2025-08-09 18:43:51 +0530

    Minor bug fix

--- app/models/M_mainfeed.php
+++ app/models/M_mainfeed.php
@@ -1,5 +1,18 @@
 <?php
 class M_mainfeed{
+    private $db;
+
+    public function __construct() {
+        $this->db = new Database();
+    }
+
+    // Method to fetch posts for the main feed
+    public function getPosts() {
+        $this->db->query("SELECT * FROM Posts ORDER BY created_at DESC");
+        return $this->db->resultSet();
+    }
+
+    // Additional methods for managing main feed can be added here...
     
 }
 ?>
