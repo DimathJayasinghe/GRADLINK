070bc394e81ee10185830012f61f5d6d7650254e
Kaveen Amarasekara <kaveenamarasekara2@gmail.com>
2025-11-10 16:48:36 +0530

    DBconfig

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'GL_db');
+    define('DB_NAME', 'gl_db');
     define('APPROOT', dirname(dirname(__FILE__)));
 
     // URL_ROOT (dynamic: works for localhost, vhost, or LAN IP)
