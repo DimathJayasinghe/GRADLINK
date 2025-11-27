2ba2380cae156651b595169b5c9a390b7232dc33
sunaliperera <sunaliperera710@gmail.com>
2025-11-27 12:18:57 +0530

    Update database password to empty string in config

--- app/config/config.php
+++ app/config/config.php
@@ -8,7 +8,7 @@
     define('DB_PORT', 3306);
     define('DB_CHARSET', 'utf8mb4');
     define('DB_USER', 'root');
-    define('DB_PASSWORD', '1234');
+    define('DB_PASSWORD', '');
     define('DB_NAME', 'gl_db');
     
     // Main database
